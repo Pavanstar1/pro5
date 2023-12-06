7a769ae93f9e64b89f7e6ba687b656c74f2f9181
Pavanstar1 <148979786+Pavanstar1@users.noreply.github.com>
2023-12-06 10:58:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+
+         function toggleBackgroundColor() {
+         var homeSection = document.getElementById('home');
+           homeSection.style.backgroundColor = homeSection.style.backgroundColor === 'rgba(0, 0, 0, 0.5)' ? '#333' : 'rgba(0, 0, 0, 0.5)';
+        }
